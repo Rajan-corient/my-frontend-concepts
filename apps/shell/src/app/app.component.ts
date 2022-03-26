38b09ae4ac01fc0a174fe04c875e57af3ad885c9
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2022-03-26 13:20:57 +0530

    monorepo skeleton created

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-frontend-concepts-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shell';
+}
